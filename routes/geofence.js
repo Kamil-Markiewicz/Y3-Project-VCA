b7a9f70a4b802fa7f421ef27b8043f91d451e90b
Dean Coakley <dean.s.coakley@gmail.com>
2018-01-04 12:41:39 +0000

    Working geofence adding

--- routes/geofence.js
+++ routes/geofence.js
@@ -1,39 +1,55 @@
 const express = require("express");
-const url = require('url');
+const url = require("url");
 const firebase = require("firebase-admin");
 const bodyParser = require("body-parser");
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const axios = require("axios");
 let router = express.Router();
 
-/* GET home page. */
+/* GET geofence page. */
 router.get("/", (req, res, next) => {
     let data = url.parse(req.url, true).query;
     let hrefQuery = "?uid="+ data.uid;
-
+    
     res.render("geofence", {title: "Add a Geofence", userQuery: hrefQuery});
 });
 
 router.post("/add", urlencodedParser, (req, res) => {
-    let addr_input = req.body.address;
-    let api_req = "https://maps.google.com/maps/api/geocode/json?address=" + addr_input;
+    let data = url.parse(req.url, true).query;
+    let hrefQuery = "?uid="+ data.uid;
+
+    let addr_input = req.body.address + " , Ireland";
+    console.log(req.body);
+    let patient_uid = req.body.patient_uid;
 
     axios.get("https://maps.google.com/maps/api/geocode/json", {
         params: {
             address: addr_input
         }
     })
-    .then(function (response) {
-        console.log(response.data.results[0].geometry);
+    .then((response) => {
+        console.log(response.data.results[0].formatted_address);
+        console.log(response.data.results[0].geometry.location.lat);
+        console.log(response.data.results[0].geometry.location.lng);
+
         let lat = response.data.results[0].geometry.location.lat;
         let long = response.data.results[0].geometry.location.lng;
 
+        // if lat is undefined, so is long. Checks if address query returned valid result
+        if (lat !== "undefined") {
+            let long_ref = firebase.database().ref("patients_flattened/" + patient_uid + "/geofenceLong");
+            long_ref.set(long);
+            let lat_ref = firebase.database().ref("patients_flattened/" + patient_uid + "/geofenceLat");
+            lat_ref.set(lat);
+        }
     })
-    .catch(function (error) {
+    .then(()=> {
+        res.render(home, {title: "Add a Geofence", userQuery: hrefQuery});
+    })
+    .catch((error) => {
         console.log(error);
+        res.render("geofence", {title: "Add a Geofence", userQuery: hrefQuery});
     });
-
-    res.render("geofence")
 });
 
 module.exports = router;
